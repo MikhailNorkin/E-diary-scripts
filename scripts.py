af683732fa3d566f1aede8e1c083bcd9e90b39c5
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-07-02 22:12:39 +0300

    Add fix_marks and remove_chastisements

--- scripts.py
+++ scripts.py
@@ -1,21 +1,37 @@
+def print_answer(texst_anser):
+	import sys
+	print(texst_anser)
+	sys.exit(1)
+
+
 def create_commendation(child_name,Subject_name):
-	from datacenter.models import Schoolkid
 	from django.core.exceptions import ObjectDoesNotExist
 	from django.core.exceptions import MultipleObjectsReturned
-	import sys
-	try:
-		e = Schoolkid.objects.filter(full_name__contains=child_name).get()
-	except ObjectDoesNotExist:
-		print("Не найдено не одного ученика с таким именем")
-		sys.exit(1)
-	except MultipleObjectsReturned:
-		return print("С таким именем слишком много учеников")
-		sys.exit(1)
-	child= Schoolkid.objects.filter(full_name__contains=child_name)[0]
 	from datacenter.models import Subject
-	Subject_year = Subject.objects.filter(title=Subject_name, year_of_study=child.year_of_study)[0]
 	from datacenter.models import Lesson
-	Lessons = Lesson.objects.filter(year_of_study = child.year_of_study, group_letter=child.group_letter, subject=Subject_year)
-	Lesson_random = random.choice(Lessons)
 	from datacenter.models import Commendation
-	Commendation.objects.create(text="Хвалю", created=Lesson_random.date,schoolkid=child,subject=Lesson_random.subject,teacher=Lesson_random.teacher)
+	from datacenter.models import Schoolkid
+	import random
+	try:
+		schoolkid_child = Schoolkid.objects.filter(full_name__contains=child_name).get()
+	except ObjectDoesNotExist:
+		print_answer("Не найдено не одного ученика с таким именем")
+	except MultipleObjectsReturned:
+		print_answer("С таким именем слишком много учеников")
+	subject = Subject.objects.filter(title=Subject_name, year_of_study=schoolkid_child.year_of_study).order_by("-year_of_study").first()
+	lesson = Lesson.objects.filter(year_of_study=schoolkid_child.year_of_study, subject=subject, group_letter=schoolkid_child.group_letter).order_by("-date").first()
+	Commendation.objects.create(text="Хвалю", created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
+
+
+def fix_marks(schoolkid):
+	from datacenter.models import Mark
+	marks_child_23 = Mark.objects.filter(schoolkid=schoolkid,points__in=[2,3])
+	for mark in marks_child_23:
+		mark.points = 5
+		mark.save()
+
+
+def remove_chastisements(schoolkid):
+	from datacenter.models import Chastisement
+	Chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+	Chastisements.delete()
