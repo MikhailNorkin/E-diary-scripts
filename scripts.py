04ab21efe741dd55de2f917d8440fd060f95085a
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-07-09 23:25:37 +0300

    Del order_by(-year_of_study)

--- scripts.py
+++ scripts.py
@@ -12,12 +12,12 @@ def find_schoolkid_child(child_name):
 	sys.exit(1)
 	
 
-def create_commendation(child_name,Subject_name):
+def create_commendation(child_name,subject_name):
 	from datacenter.models import Subject
 	from datacenter.models import Lesson
 	from datacenter.models import Commendation
 	schoolkid_child = find_schoolkid_child(child_name)
-	subject = Subject.objects.filter(title=Subject_name, year_of_study=schoolkid_child.year_of_study).order_by("-year_of_study").first()
+	subject = Subject.objects.filter(title=subject_name, year_of_study=schoolkid_child.year_of_study).first()
 	lesson = Lesson.objects.filter(year_of_study=schoolkid_child.year_of_study, subject=subject, group_letter=schoolkid_child.group_letter).order_by("-date").first()
 	Commendation.objects.create(text="Хвалю", created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
 	sys.exit(1)
