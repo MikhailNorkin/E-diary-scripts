8c6681be900b247ea9495723d3808c978b16dc0e
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-07-09 22:41:03 +0300

    del print_answer

--- scripts.py
+++ scripts.py
@@ -1,26 +1,21 @@
-def print_answer(texst_anser):
-	import sys
-	print(texst_anser)
-	sys.exit(1)
-
-
 def create_commendation(child_name,Subject_name):
-	from django.core.exceptions import ObjectDoesNotExist
-	from django.core.exceptions import MultipleObjectsReturned
+	import sys
 	from datacenter.models import Subject
 	from datacenter.models import Lesson
 	from datacenter.models import Commendation
 	from datacenter.models import Schoolkid
 	try:
-		schoolkid_child = Schoolkid.objects.filter(full_name__contains=child_name).get()
-	except ObjectDoesNotExist:
-		print_answer("Не найдено не одного ученика с таким именем")
-	except MultipleObjectsReturned:
-		print_answer("С таким именем слишком много учеников")
+		schoolkid_child = Schoolkid.objects.get(full_name__contains=child_name)
+	except Schoolkid.DoesNotExist:
+		print("Не найдено не одного ученика с таким именем")
+		sys.exit(1)
+	except Schoolkid.MultipleObjectsReturned:
+		print("С таким именем слишком много учеников")
+		sys.exit(1)
 	subject = Subject.objects.filter(title=Subject_name, year_of_study=schoolkid_child.year_of_study).order_by("-year_of_study").first()
 	lesson = Lesson.objects.filter(year_of_study=schoolkid_child.year_of_study, subject=subject, group_letter=schoolkid_child.group_letter).order_by("-date").first()
 	Commendation.objects.create(text="Хвалю", created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
-
+	
 
 def fix_marks(schoolkid):
 	from datacenter.models import Mark
