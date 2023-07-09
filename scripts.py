77edd12272e4e8af22c81d2f88394d3247175963
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-07-09 23:18:57 +0300

    Add daf find_schoolkid_child

--- scripts.py
+++ scripts.py
@@ -1,20 +1,26 @@
-def create_commendation(child_name,Subject_name):
-	import sys
-	from datacenter.models import Subject
-	from datacenter.models import Lesson
-	from datacenter.models import Commendation
+import sys
+
+def find_schoolkid_child(child_name):
 	from datacenter.models import Schoolkid
 	try:
 		schoolkid_child = Schoolkid.objects.get(full_name__contains=child_name)
+		return schoolkid_child
 	except Schoolkid.DoesNotExist:
 		print("Не найдено не одного ученика с таким именем")
-		sys.exit(1)
 	except Schoolkid.MultipleObjectsReturned:
 		print("С таким именем слишком много учеников")
-		sys.exit(1)
+	sys.exit(1)
+	
+
+def create_commendation(child_name,Subject_name):
+	from datacenter.models import Subject
+	from datacenter.models import Lesson
+	from datacenter.models import Commendation
+	schoolkid_child = find_schoolkid_child(child_name)
 	subject = Subject.objects.filter(title=Subject_name, year_of_study=schoolkid_child.year_of_study).order_by("-year_of_study").first()
 	lesson = Lesson.objects.filter(year_of_study=schoolkid_child.year_of_study, subject=subject, group_letter=schoolkid_child.group_letter).order_by("-date").first()
 	Commendation.objects.create(text="Хвалю", created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
+	sys.exit(1)
 	
 
 def fix_marks(schoolkid):
