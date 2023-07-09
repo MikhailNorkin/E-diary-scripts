6da742f925db7347824866832d5239ef6689385f
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-07-09 23:37:18 +0300

    Rename marks_child_23

--- scripts.py
+++ scripts.py
@@ -1,4 +1,11 @@
 import sys
+import random
+
+FEEDBACK = [
+	"Хвалю",
+	"Молодец",
+	"Отлично"
+]
 
 def find_schoolkid_child(child_name):
 	from datacenter.models import Schoolkid
@@ -19,14 +26,14 @@ def create_commendation(child_name,subject_name):
 	schoolkid_child = find_schoolkid_child(child_name)
 	subject = Subject.objects.filter(title=subject_name, year_of_study=schoolkid_child.year_of_study).first()
 	lesson = Lesson.objects.filter(year_of_study=schoolkid_child.year_of_study, subject=subject, group_letter=schoolkid_child.group_letter).order_by("-date").first()
-	Commendation.objects.create(text="Хвалю", created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
+	Commendation.objects.create(text=random.choice(FEEDBACK), created=lesson.date, schoolkid=schoolkid_child, subject=lesson.subject, teacher=lesson.teacher)
 	sys.exit(1)
 	
 
 def fix_marks(schoolkid):
 	from datacenter.models import Mark
-	marks_child_23 = Mark.objects.filter(schoolkid=schoolkid,points__in=[2,3])
-	for mark in marks_child_23:
+	marks_child = Mark.objects.filter(schoolkid=schoolkid,points__in=[2,3])
+	for mark in marks_child:
 		mark.points = 5
 		mark.save()
 
